84c1b0aa5db5b5c8fbebf55212ca275a1001ce1a
AahanPrajapati <73948121+AahanPrajapati@users.noreply.github.com>
2021-04-09 16:00:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("My First Console Project");
+var First_Name="Aahan";
+var Last_Name="Prajapati";
+console.log(First_Name.concat(Last_Name));
+var N1=123
+var N2=243
+console.log(N1+N2);
+console.log(N1-N2);
+console.log(N1*N2);
+console.log(N1/N2);
